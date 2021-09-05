d9424b0887994036e5870682d8d35f1ab7d0206c
Namju <skawn11114@gmail.com>
2021-09-06 02:01:06 +0900

    feat: publish settings

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import PaginationLayout from "./components/PaginationLayout";
+import PaginationLayout from "./lib/NYAM_PAGINATION";
 import YourComponent from "./components/YourComponent";
 import { justEngAndCode as yourDataItems } from "./utils/mockData";
 
 function App() {
   return (
-    <div className="App">
-      <PaginationLayout
-        Component={YourComponent}
-        itemsPerPage={5}
-        paginationCount={10}
-        yourDataItems={yourDataItems}
-      />
-    </div>
+    <PaginationLayout
+      Component={YourComponent}
+      itemsPerPage={5}
+      paginationCount={10}
+      yourDataItems={yourDataItems}
+    />
   );
 }
 export default App;
