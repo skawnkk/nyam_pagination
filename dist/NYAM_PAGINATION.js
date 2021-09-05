d9d187f6128cd22ec109d06d20334b2db8005453
Namju <skawn11114@gmail.com>
2021-09-06 03:10:15 +0900

    feat. npm publish & successgit push origin master

---
+++ dist/NYAM_PAGINATION.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect, useCallback } from "react";
+import styled from "styled-components";
+export const NAME = {
+  FIRST: `<< First`,
+  PREV: `< Prev`,
+  NEXT: `Next >`,
+  END: `End  >>`
+};
+export default function PaginationLayout({
+  Component,
+  itemsPerPage,
+  paginationCount,
+  yourDataItems
+}) {
+  const [dataToShow, setDataToShow] = useState([]);
+  const [wholePages, setWholePages] = useState(0);
+  const [pageNum, setPageNum] = useState(1);
+  const dividedPageItems = useCallback(data => {
+    if (!data.length) return [];
+    const pageItems = [];
+
+    for (let i = 0; i < data.length; i += itemsPerPage) {
+      const pageUsers = data.slice(i, i + itemsPerPage);
+      pageItems.push(pageUsers);
+    }
+
+    return pageItems;
+  }, [itemsPerPage]);
+  useEffect(() => {
+    const pagedItem = dividedPageItems(yourDataItems);
+    setWholePages(pagedItem.length);
+    setDataToShow(pagedItem[pageNum - 1] || []);
+  }, [pageNum, dividedPageItems, yourDataItems]); //pagination
+
+  const [paginationBtns, setPaginationBtns] = useState([]);
+  const changeMainPage = useCallback(pageNum => {
+    const totalPageLists = Array.from({
+      length: wholePages
+    }, (_, idx) => idx + 1);
+    const remains = wholePages - pageNum;
+    const half = paginationCount / 2;
+    if (wholePages <= paginationCount) return totalPageLists.slice(0, wholePages);
+    if (pageNum <= half) return totalPageLists.slice(0, paginationCount);
+    return pageNum > half && remains <= half ? totalPageLists.slice(pageNum - paginationCount + remains, wholePages) : totalPageLists.slice(pageNum - half - 1, pageNum + half - 1);
+  }, [wholePages, paginationCount]);
+
+  const handlePage = (e, page) => {
+    const type = e.target.id;
+    if (type === "prev") setPageNum(prevPage => prevPage - 1);
+    if (type === "next") setPageNum(prevPage => prevPage + 1);
+    if (type === "first") setPageNum(1);
+    if (type === "end") setPageNum(wholePages);
+    if (type === "pagination") setPageNum(page);
+  };
+
+  useEffect(() => {
+    const currentPageMainLists = changeMainPage(pageNum);
+    setPaginationBtns(currentPageMainLists);
+  }, [changeMainPage, pageNum, wholePages]);
+  const {
+    FIRST,
+    PREV,
+    NEXT,
+    END
+  } = NAME;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Component, {
+    dataToShow: dataToShow
+  }), /*#__PURE__*/React.createElement(PaginationWrapper, null, wholePages !== 0 && /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(OptionBtn, {
+    id: "first",
+    onClick: handlePage,
+    disabled: pageNum === 1
+  }, FIRST), /*#__PURE__*/React.createElement(OptionBtn, {
+    id: "prev",
+    onClick: handlePage,
+    disabled: pageNum === 1
+  }, PREV)), /*#__PURE__*/React.createElement(AlignPages, null, paginationBtns.map((page, idx) => /*#__PURE__*/React.createElement(PaginationBtn, {
+    key: idx,
+    id: "pagination",
+    page: page,
+    pageNum: pageNum,
+    onClick: e => handlePage(e, page)
+  }, page))), wholePages !== 0 && /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(OptionBtn, {
+    id: "next",
+    onClick: handlePage,
+    disabled: pageNum === wholePages
+  }, NEXT), /*#__PURE__*/React.createElement(OptionBtn, {
+    id: "end",
+    onClick: handlePage,
+    disabled: pageNum === wholePages
+  }, END))));
+}
+export const theme = {
+  color: {
+    borderline: "#B6AFAF",
+    button: "#AAC14F",
+    Black: "#333333",
+    White: "#FFFFFF"
+  }
+};
+const flexDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const PaginationWrapper = styled(flexDiv)`
+  margin: 15px 0;
+`;
+const PaginationBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  border: 0.5px solid ${theme.color.borderline};
+  border-radius: 3px;
+  margin-right: 5px;
+  background-color: ${({
+  page,
+  pageNum = -1
+}) => page === pageNum ? theme.color.button : theme.color.White};
+  color: ${({
+  page,
+  pageNum = -1
+}) => page === pageNum ? theme.color.White : theme.color.Black};
+
+  cursor: pointer;
+`;
+const OptionBtn = styled(PaginationBtn)`
+  width: 60px;
+`;
+const AlignPages = styled(flexDiv)`
+  margin: auto 20px;
+`;
